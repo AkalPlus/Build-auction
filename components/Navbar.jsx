a9f6539b9ffddb1f3f74685e90287dff1ed4f5f9
priyanshukr449 <priyanshukr449@gmail.com>
2024-03-19 22:35:28 +0530

    Hero section added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,27 @@
+"use client";
+
 import React from "react";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // get location
+  // const getLocationHandler = async () => {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       console.log(position);
+  //     },
+  //     () => {
+  //       console.log("error");
+  //     },
+  //   );
+  // };
+
+  return (
+    <div>
+      Navbar
+      {/* <Button onClick={getLocationHandler}>Get Location</Button> */}
+    </div>
+  );
 };
 
 export default Navbar;
