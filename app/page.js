7b85dcfb91af2b7014cb1bb266fd6f061b6ebd68
turingAlan <sarthak2107597@st.jmi.ac.in>
2024-03-28 22:38:40 +0530

    chore

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
-import Navbar from "@/components/Navbar";
-import Image from "next/image";
+import Navbar from '@/components/Navbar'
+import {
+  LoginLink,
+  RegisterLink,
+} from '@kinde-oss/kinde-auth-nextjs/components'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
+    <div className="h-[200vh]">
       <Navbar />
-    </>
-  );
+      <LoginLink>Sign in</LoginLink>
+
+      <RegisterLink>Sign up</RegisterLink>
+    </div>
+  )
 }
