f40ee09b88045e54772c604faf747893b7d2e8db
turingAlan <sarthak2107597@st.jmi.ac.in>
2024-03-29 22:25:17 +0530

    feat: migration complete

--- app/page.js
+++ app/page.js
@@ -3,7 +3,12 @@ import CTA from '@/components/CTA'
 import Faq from '@/components/Faq'
 import HeroSection from '@/components/HeroSection'
 import Navbar from '@/components/Navbar'
+import { Button } from '@/components/ui/button'
 import WorkList from '@/components/WorkList'
+import {
+  LoginLink,
+  RegisterLink,
+} from '@kinde-oss/kinde-auth-nextjs/components'
 
 export default function Home() {
   return (
@@ -13,6 +18,15 @@ export default function Home() {
       <WorkList />
       <ContractorLists />
       <CTA />
+
+      <div className="flex justify-around bg-black">
+        <LoginLink postLoginRedirectURL="/">
+          <Button variant="primary">Sign in</Button>
+        </LoginLink>
+        <RegisterLink postLoginRedirectURL="/">
+          <Button variant="primary">Sign up</Button>
+        </RegisterLink>
+      </div>
       <Faq />
     </>
   )
