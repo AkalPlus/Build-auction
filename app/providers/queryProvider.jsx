2c028fff51e89867a6abd8ca2ab24fa361399734
turingAlan <sarthak2107597@st.jmi.ac.in>
2024-05-27 22:28:37 +0530

    feat(app): react query fixes

--- app/providers/queryProvider.jsx
+++ app/providers/queryProvider.jsx
@@ -1,17 +1,10 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools'
 import { useState } from 'react'
 
 export const QueryProvider = ({ children }) => {
   const [query] = useState(() => new QueryClient())
 
-  return (
-    <QueryClientProvider client={query}>
-      <ReactQueryDevtools initialIsOpen={false} />
-
-      {children}
-    </QueryClientProvider>
-  )
+  return <QueryClientProvider client={query}>{children}</QueryClientProvider>
 }
